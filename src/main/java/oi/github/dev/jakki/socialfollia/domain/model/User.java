5b5b3fa8b5821055a48fa2c60ca2bbf22d682be1
dev-jakki <jackcielfelix.dev@gmail.com>
2026-07-12 15:51:38 -0300

    feat: cria as rotas de inserir e listar usuários

---
+++ src/main/java/oi/github/dev/jakki/socialfollia/domain/model/User.java
@@ -0,0 +1,57 @@
+package oi.github.dev.jakki.socialfollia.domain.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users") // Nome da tabela que faz referência
+public class User extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
--- src/main/java/oi/github/dev/jakki/socialfollia/rest/UserResource.java
+++ src/main/java/oi/github/dev/jakki/socialfollia/rest/UserResource.java
@@ -1,15 +1,35 @@
 package oi.github.dev.jakki.socialfollia.rest;
 
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import oi.github.dev.jakki.socialfollia.domain.model.User;
+import oi.github.dev.jakki.socialfollia.rest.dto.CreateUserRequest;
 
 @Path("/users")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
-    public Response create() {
-        return Response.ok().build();
+    @Transactional // Sempre usar quando for fazer alguma ALTERAÇÃO no banco de dados (Visualizar/Listar informações não se encaixam)
+    public Response create(CreateUserRequest userRequest) {
+        User user = new User();
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+
+        user.persist();
+
+        return Response.ok(user).build();
+    }
+
+    @GET
+    public Response listAll() {
+        PanacheQuery<User> query = User.findAll();
+        return Response.ok(query.list()).build();
     }
 
 }
