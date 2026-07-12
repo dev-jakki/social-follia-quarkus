60616ba59831c0a32f2c28c28755d3223ce64e84
dev-jakki <jackcielfelix.dev@gmail.com>
2026-07-12 17:54:51 -0300

    feat: cria endpoints deletar e atualizar

--- src/main/java/oi/github/dev/jakki/socialfollia/rest/UserResource.java
+++ src/main/java/oi/github/dev/jakki/socialfollia/rest/UserResource.java
@@ -9,13 +9,15 @@ import jakarta.ws.rs.core.Response;
 import oi.github.dev.jakki.socialfollia.domain.model.User;
 import oi.github.dev.jakki.socialfollia.rest.dto.CreateUserRequest;
 
+import java.util.Optional;
+
 @Path("/users")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
-    @Transactional // Sempre usar quando for fazer alguma ALTERAÇÃO no banco de dados (Visualizar/Listar informações não se encaixam)
+    @Transactional
     public Response create(CreateUserRequest userRequest) {
         User user = new User();
         user.setName(userRequest.getName());
@@ -30,6 +32,38 @@ public class UserResource {
     public Response listAll() {
         PanacheQuery<User> query = User.findAll();
         return Response.ok(query.list()).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        User user = User.findById(id);
+
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        user.delete();
+        return Response.ok().build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Transactional
+    public Response update(@PathParam("id") Long id, CreateUserRequest userRequest) {
+        User user = User.findById(id);
+
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        user.setName(userRequest.getName());
+        user.setAge(userRequest.getAge());
+
+        user.persist();
+
+        return Response.ok(user).build();
     }
 
 }
